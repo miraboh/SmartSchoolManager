4566e183ba3227bd9e43980f5bebf55813e1a4c1
miraboh <35488181+miraboh@users.noreply.github.com>
2020-10-10 18:37:25 +0100

    Revert "refactoring (#5)"
    This reverts commit d20083acbd7cf47808dc467dc474cad4420a25bb.

--- app/src/main/java/com/mobileedu02/smartschoolmanager/HomeFragment.kt
+++ app/src/main/java/com/mobileedu02/smartschoolmanager/HomeFragment.kt
@@ -1,15 +1,16 @@
-package com.mobileedu02.smartschoolmanager.ui.fragments.home
+package com.mobileedu02.smartschoolmanager
 
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.mobileedu02.smartschoolmanager.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
     companion object {
-        fun newInstance() =
-            HomeFragment()
+        fun newInstance() = HomeFragment()
     }
 
     private lateinit var viewModel: HomeViewModel
