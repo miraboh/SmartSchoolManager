028aad136239c69c8d8150b70d412881dff26023
miraboh <MIRAboh581>
2020-10-11 09:42:11 +0100

    squash! refactoring (#5)

--- app/src/main/java/com/mobileedu02/smartschoolmanager/ui/fragments/auth/signout/SignOutViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.mobileedu02.smartschoolmanager.ui.fragments.auth.signout
-
-import androidx.lifecycle.ViewModel
-
-class SignOutViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/mobileedu02/smartschoolmanager/ui/fragments/splashscreen/SplashScreenViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.mobileedu02.smartschoolmanager.ui.fragments.splashscreen
-
-import androidx.lifecycle.ViewModel
-
-class SplashScreenViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
