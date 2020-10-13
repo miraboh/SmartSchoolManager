042cbf4a2e9fb927427d402143e2c418db27f583
miraboh <MIRAboh581>
2020-10-13 02:48:42 +0100

    Library section almost done

--- app/src/main/java/com/mobileedu02/smartschoolmanager/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/mobileedu02/smartschoolmanager/ui/fragments/home/HomeFragment.kt
@@ -3,13 +3,14 @@ package com.mobileedu02.smartschoolmanager.ui.fragments.home
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.mobileedu02.smartschoolmanager.R
 import com.mobileedu02.smartschoolmanager.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
     companion object {
-        fun newInstance() =
-            HomeFragment()
+        fun newInstance() = HomeFragment()
     }
 
     private lateinit var viewModel: HomeViewModel
@@ -21,6 +22,10 @@ class HomeFragment : Fragment() {
     ): View? {
 
         binding = HomeFragmentBinding.inflate(inflater)
+
+        binding.libraryId.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_libraryFragment)
+        }
 
         return binding.root
     }
