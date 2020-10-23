09f175a82d15545e38ed7420dab9058bb711179a
Lawrence <law.ebo33@gmail.com>
2020-10-23 04:42:54 +0100

    about us tab view

---
+++ app/src/main/java/com/mobileedu02/smartschoolmanager/ui/activity/AboutUs.kt
@@ -0,0 +1,29 @@
+package com.mobileedu02.smartschoolmanager.ui.activity
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.tabs.TabLayout
+import androidx.viewpager.widget.ViewPager
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.google.android.material.appbar.AppBarLayout
+import com.mobileedu02.smartschoolmanager.R
+import com.mobileedu02.smartschoolmanager.ui.activity.ui.main.SectionsPagerAdapter
+import kotlinx.android.synthetic.main.activity_about_us.*
+
+class AboutUs : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+        back_navigation.setOnClickListener{finish()}
+    }
+}
