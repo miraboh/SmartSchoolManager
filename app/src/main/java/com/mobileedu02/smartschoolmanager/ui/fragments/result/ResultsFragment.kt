c113b2a0a357ebfda98d961c5c1b922ea81488bb
Lawrence <law.ebo33@gmail.com>
2020-10-29 05:18:32 +0100

    result_fragment_update

--- app/src/main/java/com/mobileedu02/smartschoolmanager/ui/fragments/result/ResultsFragment.kt
+++ app/src/main/java/com/mobileedu02/smartschoolmanager/ui/fragments/result/ResultsFragment.kt
@@ -1,15 +1,22 @@
 package com.mobileedu02.smartschoolmanager.ui.fragments.result
 
 import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
+import com.mobileedu02.smartschoolmanager.R
 import com.mobileedu02.smartschoolmanager.databinding.FragmentResultsBinding
 import com.mobileedu02.smartschoolmanager.model.Users
+import com.mobileedu02.smartschoolmanager.ui.activity.AboutUs
+import com.mobileedu02.smartschoolmanager.ui.fragments.quiz.QuizFragment
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_results.*
 
 class ResultsFragment : Fragment() {
 
@@ -50,10 +57,29 @@ class ResultsFragment : Fragment() {
                 val user = dataSnapshot.getValue(Users::class.java)
                 mProgressBar!!.hide()
 
-                // Ebo this is the object to be displayed
-                binding.txtResult.text = user!!.score.toString()
-                //you can add other stuffs like the name like below etc
-                //binding.txtResult.text = user.surName.toString()
+                if (user == null) {
+                    binding.txtResult.text = getString(R.string.no_score_message)
+                } else{
+                    val userResultMessage = "Hello ${user.otherNames.toString()}, you scored ${user.score.toString()} out of 20"
+                    binding.txtResult.text = userResultMessage
+
+                    val userReward = when (user.score){
+                        in 15..20 -> "excellent performance"
+                        in 8..14 -> "Good score but you can do better"
+                        else -> "You performed quite poorly"
+                    }
+                    txtReward.text = userReward
+
+                    val emoji = when (user.score){
+                        in 15..20 -> R.drawable.emoji_excellent
+                        in 8..14 -> R.drawable.emoji_happy
+                        else -> R.drawable.emoji_sad
+                    }
+                    img_view_emoji.setImageResource(emoji)
+
+                    if (user.score in 0..14 ) {try_again_btn.visibility = View.VISIBLE}
+                    try_again_btn.setOnClickListener { findNavController().navigate(R.id.quizFragment) }
+                }
 
             }
 
@@ -63,4 +89,6 @@ class ResultsFragment : Fragment() {
         }
         mDatabaseReference!!.child(userId!!).addListenerForSingleValueEvent(userListener)
     }
+
+
 }
